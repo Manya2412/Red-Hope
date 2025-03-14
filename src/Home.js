ab95cdd93b370c7a1efc1f2691df3abe0203f845
Manya2412 <151117108+Manya2412@users.noreply.github.com>
2025-03-15 00:17:11 +0530

    Update

--- src/Home.js
+++ src/Home.js
@@ -8,16 +8,28 @@ import { useState, useEffect } from "react";
 // Our Services
 const OurServices = () => {
     const services = [
-      { name: "Extensive Blood Donor Database", dark: "/Donor Database.png",   light: "Donor Database.png" },
-      { name: "Instant Donor ID Access", dark: "ID Access.png", light: "ID Access.png" },
-      { name: "Safe Donations with Top Hospitals", dark: "Safe donations.png", light: "Safe donations.png" },
-      { name: "Automated Reminders for Donors", dark: "Automated Reminders.png", light: "Automated Reminders.png" },
-      { name: "No More Family Dependence for Blood", dark: "Family Dependence.png", light: "Family Dependence.png" },
-      { name: "Instant Emergency Blood Fulfillment", dark: "Emergency Blood.png", light: "Emergency Blood.png" },
+      {
+        name: "Extensive Blood Donor Database", dark: "/donor-database.png", light: "/donor-database.png",
+      },
+      {
+        name: "Instant Donor ID Access", dark: "/id-access.png", light: "/id-access.png",
+      },
+      { 
+        name: "Safe Donations with Top Hospitals", dark: "/safe-donations.png", light: "/safe-donations.png",
+      },
+      {
+        name: "Automated Reminders for Donors", dark: "/donation-day.png", light: "/donation-day.png",
+      },
+      {
+        name: "No More Family Dependence for Blood", dark: "/family-dependence.png", light: "/family-dependence.png",
+      },
+      {
+        name: "Instant Emergency Blood Fulfillment", dark: "/emergency-blood.png", light: "/emergency-blood.png",
+      },
     ];
   
     return (
-        <section className="services">
+      <section className="services">
         <h1 className="heading-title">Our Services</h1>
         <div className="box-container">
           {services.map((service, index) => (
@@ -39,3 +51,5 @@ const OurServices = () => {
   };
   
   export default OurServices;
+
+  
